91d3ecc1d393954768535d5c11a843b9cdfbca2a
GioZhorzholiani3 <giorgi.zhorzholiani.1@btu.edu.ge>
2023-01-28 16:20:53 +0400

    add simple data file

---
+++ src/assets/Data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    email: "",
+  },
+];
+
+export default data;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ProductListHeader from "../components/Header/ProductListHeader";
+import data from "../assets/Data";
 
 const ProductList = () => {
   return (
     <div>
       <ProductListHeader />
+      {data.map((item) => {
+        return <p>{item.name}</p>;
+      })}
     </div>
   );
 };
